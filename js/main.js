0fbcd1bb3698d760fa4d93a194f6f957e2960077
Narek <narek700@mail.ru>
2022-06-02 01:10:43 +0400

    change cv sad

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ let deBouncer = function () {
     tid = setTimeout(function () {
         resizeReset();
     }, delay);
-};``
+};
 
 let checkDistance = function (x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
